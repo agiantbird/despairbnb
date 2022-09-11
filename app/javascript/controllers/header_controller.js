9d73be92cc6cc5765ca052a3d98711ac6e1c4f4b
Rich Hoyt <rhoyt10@gmail.com>
2022-09-11 13:20:23 -0400

    Add dropdown profile menu

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -2,10 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 import {enter, leave, toggle} from 'el-transition'
 
 export default class extends Controller {
+  static targets = ['openUserMenu'];
   connect() {
     // this.element.textContent = "Bye World!"
-    console.log('enter: ', enter);
-    console.log('leave: ', leave);
-    console.log('toggle: ', toggle);
+    // console.log('enter: ', enter);
+    // console.log('leave: ', leave);
+    // console.log('toggle: ', toggle);
+    this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
+  }
+
+  toggleDropdownMenu() {
+    toggle(document.getElementById('menu-dropdown-items'));
   }
 }
