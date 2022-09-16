8f65770488e9ee418b407d26f3fc8d7d14097cb1
Rich Hoyt <rhoyt10@gmail.com>
2022-09-16 18:47:27 -0400

    Add user show method and spec

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "home#index"
+
+  namespace :api do
+    resources :users, only: :show
+  end
 end
