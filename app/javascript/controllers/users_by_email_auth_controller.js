4432459340bf11a353188a7ef34ae8f98bd4fe0b
Rich Hoyt <rhoyt10@gmail.com>
2022-09-19 20:29:45 -0400

    Remove old user api, add axios http library

--- app/javascript/controllers/users_by_email_auth_controller.js
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -1,8 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
+import axios from 'axios';
 
 export default class extends Controller {
     static targets = ['email', 'submit'];
     connect() {
+        console.log('axios: ', axios)
         this.submitTarget.addEventListener('click', (e) => {
             e.preventDefault();
 
@@ -11,7 +13,19 @@ export default class extends Controller {
                 //email field is empty so don't do anything
             } else {
                 // email field is filled out so do something
-                console.log('hey we have content')
+                // console.log('hey we have content')
+                axios.get('/api/users_by_email', {
+                    params: {
+                        email: this.emailTarget.value
+                    },
+                    headers : {
+                        'ACCEPT': 'application/json'
+                    }
+                }).then((response) => {
+                    Turbo.visit('/users/sign_in');
+                }).catch((response) => {
+                    Turbo.visit('/users/sign_up');
+                })
             }
         });
     }
