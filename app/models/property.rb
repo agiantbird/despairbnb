30b62fe9332d48d1439f2079f050220d40fa98d3
Rich Hoyt <rhoyt10@gmail.com>
2022-09-26 12:41:04 -0400

    Get Geocoder working with Faker data

--- app/models/property.rb
+++ app/models/property.rb
@@ -7,4 +7,12 @@ class Property < ApplicationRecord
   validates :city, presence: true
   validates :state, presence: true
   validates :country, presence: true
+
+  # Geocoder
+  geocoded_by :address
+  after_validation :geocode, if: ->(obj){ obj.latitude.blank? && obj.longitude.blank? }
+
+  def address
+    [state, country].compact.join(', ')
+  end
 end
