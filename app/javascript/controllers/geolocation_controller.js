ee53ae2126139367e53c9f507c47255d06009a47
Rich Hoyt <rhoyt10@gmail.com>
2022-09-28 16:55:40 -0400

    Calculate distance from user's geolocation

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,11 +1,29 @@
 import { Controller } from '@hotwired/stimulus'
+import { getDistance, convertDistance } from 'geolib';
 
 export default class extends Controller {
+    static targets = ['property']
     connect() {
-        console.log('this.element: ', this.element);
         window.navigator.geolocation.getCurrentPosition((position) => {
             this.element.dataset.latitude = position.coords.latitude
             this.element.dataset.longitude = position.coords.longitude
+
+            const distance = getDistance(
+                { latitude: position.coords.latitude, longitude: position.coords.longitude },
+                { latitude: 61.36031, longitude: -153.71580 }
+            )
+
+            const distanceInKm = convertDistance(distance, 'km');
+            console.log('distance: ', distanceInKm);
+
+            this.propertyTargets.forEach((propertyTarget) => {
+                let distanceFrom = getDistance(
+                    { latitude: position.coords.latitude, longitude: position.coords.longitude},
+                    { latitude: propertyTarget.dataset.latitude, longitude: propertyTarget.dataset.longitude} ,
+                );
+                console.log('distanceFrom ', convertDistance(distanceFrom, 'km'));
+                propertyTarget.querySelector('[data-distance-away]').innerHTML = `${Math.ceil(convertDistance(distanceFrom, 'km'))} km away`;
+            });
         });
     }
 }
