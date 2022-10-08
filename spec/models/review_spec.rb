818f8385b4981d7f01e4baa94323922695489ee3
Rich Hoyt <rhoyt10@gmail.com>
2022-10-08 14:30:30 -0400

    Add associations, validations, and specs to reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:rating) }
+  it { should belong_to(:reviewable) }
+  it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(1) }
+  it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5) }
+  it { should validate_numericality_of(:rating).only_integer }
 end
