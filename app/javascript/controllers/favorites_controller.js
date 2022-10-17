a1ef5debd057de2586e7c0148503ecd9e5461174
Rich Hoyt <rhoyt10@gmail.com>
2022-10-17 12:49:49 -0400

    Show logged-out users who favorite property login form

--- app/javascript/controllers/favorites_controller.js
+++ app/javascript/controllers/favorites_controller.js
@@ -2,6 +2,9 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
     favorite() {
+        if(this.element.dataset.userLoggedIn === "false") {
+           return document.querySelector('[data-header-target="userAuthLink"]').click();
+        }
         if(this.element.dataset.favorited === 'true') {
             this.element.dataset.favorited = false;
             this.element.setAttribute('fill', '#CED4DA');
