b1f658094094bd37d80ff189f25cd68581052476
Rich Hoyt <rhoyt10@gmail.com>
2022-11-11 13:18:53 -0500

    Add cleaning fee and service fee percentage to property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 class Property < ApplicationRecord
+  CLEANING_FEE = 5_000.freeze
+  CLEANING_FEE_MONEY = Money.new(CLEANING_FEE)
+  SERVICE_FEE_PERCENTAGE = (0.08).freeze
   # Validations
   validates :name, presence: true
   validates :headline, presence: true
