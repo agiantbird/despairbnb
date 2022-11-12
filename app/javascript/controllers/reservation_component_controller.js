70a8e293a42fbb2004d7978498dcec3e5e6a1d76
Rich Hoyt <rhoyt10@gmail.com>
2022-11-12 14:47:09 -0500

    Don't allow checkin date in past

--- app/javascript/controllers/reservation_component_controller.js
+++ app/javascript/controllers/reservation_component_controller.js
@@ -4,11 +4,8 @@ import { Datepicker } from 'vanillajs-datepicker';
 export default class extends Controller {
     static targets = ['checkin', 'checkout'];
     connect() {
-        console.log('Inside reservation component controller');
-        console.log('Datepicker: ', Datepicker);
-
-        new Datepicker(this.checkinTarget);
-        new Datepicker(this.checkoutTarget);
-
+        const checkinPicker = new Datepicker(this.checkinTarget, {
+            minDate: '11/12/2022'
+        });
     }
 }
