9044de31e483f8de6d56d31ecca860b713a3f24a
Rich Hoyt <rhoyt10@gmail.com>
2022-11-13 21:01:54 -0500

    Add default checkin and checkout dates

--- app/javascript/controllers/reservation_component_controller.js
+++ app/javascript/controllers/reservation_component_controller.js
@@ -2,10 +2,14 @@ import { Controller } from '@hotwired/stimulus'
 import { Datepicker } from 'vanillajs-datepicker';
 
 export default class extends Controller {
-    static targets = ['checkin', 'checkout'];
+    static targets = ['checkin', 'checkout']
     connect() {
         const checkinPicker = new Datepicker(this.checkinTarget, {
-            minDate: '11/12/2022'
+            minDate: this.element.dataset.defaultCheckinDate
+        });
+
+        const checkoutPicker = new Datepicker(this.checkoutTarget, {
+            minDate: this.element.dataset.defaultCheckoutDate
         });
     }
 }
