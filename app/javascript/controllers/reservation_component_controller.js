8aa12ec17beeca9de3498d63c7b8f38c536140e7
Rich Hoyt <rhoyt10@gmail.com>
2022-11-14 21:15:26 -0500

    Make allowed check in and check out dates dependent on each other

--- app/javascript/controllers/reservation_component_controller.js
+++ app/javascript/controllers/reservation_component_controller.js
@@ -11,5 +11,21 @@ export default class extends Controller {
         const checkoutPicker = new Datepicker(this.checkoutTarget, {
             minDate: this.element.dataset.defaultCheckoutDate
         });
+
+        this.checkinTarget.addEventListener('changeDate', (e) => {
+            const date = new Date(e.target.value);
+            date.setDate(date.getDate() + 1);
+            checkoutPicker.setOptions({
+                minDate: date
+            });
+        });
+
+        this.checkoutTarget.addEventListener('changeDate', (e) => {
+            const date = new Date(e.target.value);
+            date.setDate(date.getDate() - 1);
+            checkinPicker.setOptions({
+                maxDate: date
+            });
+        });
     }
 }
