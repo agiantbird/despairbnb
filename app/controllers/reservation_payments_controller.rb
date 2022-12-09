8e275335ec535bc02d19a07d7ffe022761332d51
Rich Hoyt <rhoyt10@gmail.com>
2022-12-08 21:29:22 -0500

    Add to reservation model and payments controller

--- app/controllers/reservation_payments_controller.rb
+++ app/controllers/reservation_payments_controller.rb
@@ -1,17 +1,50 @@
 # frozen_string_literal: true
 class ReservationPaymentsController < ApplicationController
   def create
-    raise
+    charge = Stripe::Charge.create(
+      amount: Money.from_amount(BigDecimal(payment_params[:total])).cents,
+      currency: "usd",
+      source: payment_params[:stripeToken],
+      customer: stripe_customer.id
+    )
+
+    reservation = Reservation.create(
+      property: property,
+      user: user,
+      checkin_date: checkin_date,
+      checkout_date: checkout_date
+    )
+
+    payment = Payment.create(
+      reservation: reservation,
+      subtotal: Money.from_amount(BigDecimal(payment_params[:subtotal])),
+      cleaning_fee: Money.from_amount(BigDecimal(payment_params[:cleaning_fee])),
+      service_fee: Money.from_amount(BigDecimal(payment_params[:service_fee])),
+      total: Money.from_amount(BigDecimal(payment_params[:total])),
+
+    )
   end
 
   private
-  def payments_params
+  def payment_params
     params.permit(:stripeToken, :property_id, :user_id, :checkin_date, :checkout_date,
                   :subtotal, :cleaning_fee, :service_fee, :total)
   end
 
+  def property
+    @property ||= Property.find(payment_params[:property_id])
+  end
+
+  def checkin_date
+    checkin_date ||= Date.strptime(payment_params[:checkin_date].to_s)
+  end
+
+  def checkout_date
+    checkout_date ||= Date.strptime(payment_params[:checkout_date].to_s)
+  end
+
   def user
-    @user ||= User.find(params[:user_id])
+    @user ||= User.find(payment_params[:user_id])
   end
 
   def stripe_customer
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,6 +3,9 @@ class Reservation < ApplicationRecord
   belongs_to :user
   has_one :payment, dependent: :destroy
 
+  validates :checkin_date, presence: true
+  validates :checkout_date, presence: true
+
   # validates :reservation_date, presence: true
 
   # scope :future_reservations, -> { where("reservation_date > ?", Date.today).order(:reservation_date) }
