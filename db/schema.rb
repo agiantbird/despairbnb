25038e6b34919ff850967df4739dcd603e35b4a2
Rich Hoyt <rhoyt10@gmail.com>
2022-12-22 17:33:22 -0500

    Add first and last name to profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_10_024934) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_22_222505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_10_024934) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "zip_code"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
