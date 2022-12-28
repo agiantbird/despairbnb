05de85530a1bb075ca629b0985bbb3244dfd2938
Rich Hoyt <rhoyt10@gmail.com>
2022-12-28 10:25:53 -0500

    Add country code to profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_23_153833) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_28_152408) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_23_153833) do
     t.string "zip_code"
     t.string "first_name"
     t.string "last_name"
+    t.string "country_code"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
